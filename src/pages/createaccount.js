abe0c495e1ef18daab41fbf3a8bcb6cfe8e48d90
tdgog <65415295+tdgog@users.noreply.github.com>
2023-01-16 12:15:02 +0000

    now able to create account

--- src/pages/createaccount.js
+++ src/pages/createaccount.js
@@ -1,3 +1,86 @@
+import {TextField} from "@mui/material";
+import {useState, useContext} from "react";
+import {UserContext} from "../utils/usermanager";
+
+function HalfScreen({ children, className }) {
+    return <div className={`${className} h-full w-1/2`} children={children} />
+}
+
+function TextDivider({children}) {
+    function HalfDivider() {
+        return <div className={'h-[2px] w-64 bg-black'} />
+    }
+
+    return <div className={'my-5 flex items-center justify-center space-x-3'}>
+        <HalfDivider />
+        <p>{children}</p>
+        <HalfDivider />
+    </div>
+}
+
 export default function CreateAccount() {
-    return <p>ye</p>
+    const userContext = useContext(UserContext);
+
+    const [email, setEmail] = useState(null);
+    const [emailError, setEmailError] = useState(null);
+    function handleEmailUpdate(e) {
+        setEmail(e.target.value);
+
+        if(!email.includes('@') || !email.includes('.')) {
+            setEmailError('Invalid email');
+        } else {
+            setEmailError(null);
+        }
+    }
+
+    const [password, setPassword] = useState(null);
+    const [passwordError, setPasswordError] = useState(null);
+    function handlePasswordUpdate(e) {
+        setPassword(e.target.value);
+
+        if(password.length < 8) {
+            setPasswordError('Password must be longer than 8 characters');
+        } else {
+            setPasswordError(null)
+        }
+    }
+
+    return <div className={'h-screen w-screen flex justify-center items-center'}>
+        <HalfScreen className={'bg-blue-500'}>
+
+        </HalfScreen>
+        <HalfScreen className={'p-5 py-36 flex items-center flex-col'}>
+            <h1 className={'text-4xl font-title font-bold'}>Create your Account</h1>
+            <h2 className={'text-xl font-subtitle text-neutral-600'}>Enter the fields below to get started</h2>
+            <TextDivider>OR</TextDivider>
+            <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                onChange={handleEmailUpdate}
+                error={emailError !== null}
+                helperText={`${emailError || ''}`}
+                sx={{ width: '70%', marginBottom: '10px' }}
+                required
+            />
+            <TextField
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                onChange={handlePasswordUpdate}
+                error={passwordError !== null}
+                helperText={`${passwordError || ''}`}
+                sx={{ width: '70%' }}
+                required
+            />
+            <div
+                className={'form-input-button'}
+                onClick={() => {
+                    userContext[1](email, password);
+                }
+            }>
+                Create account
+            </div>
+        </HalfScreen>
+    </div>
 }
--- src/utils/usermanager.js
+++ src/utils/usermanager.js
@@ -1,17 +1,46 @@
 import React, {useState} from "react";
+import bcrypt from 'bcryptjs';
 
 export const UserContext = React.createContext([null, () => {}]);
 
 export default function UserManager({children}) {
     const [user, setUser] = useState(null);
 
-    function updateUser(value) {
-        // Validation
+    function createUser(email, password) {
+        fetch(`http://localhost:8000/isUnique`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if(!data.unique) return;
 
-        setUser(value);
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(password, salt, function (err, hash) {
+                        // Send email, hash, and salt to database
+                        const requestOptions = {
+                            method: 'POST',
+                            mode: 'cors',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({ email: email, hash: hash })
+                        };
+                        fetch(`http://localhost:8000/addUser`, requestOptions)
+                            .then(response => response.json())
+                            .then(data => {
+                                console.log(data);
+                            })
+                    })
+                })
+            })
     }
 
-    return <UserContext.Provider value={[user, updateUser]}>
+    return <UserContext.Provider value={[user, createUser]}>
         {children}
     </UserContext.Provider>
 }
